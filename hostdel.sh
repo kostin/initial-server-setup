6142279ca0b2c92b65a3a844078925af5d352be9
Alexey Kostin <kostin@users.noreply.github.com>
2015-01-28 23:38:45 +0300

    Create hostdel.sh

---
+++ hostdel.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+usage="To remove user you need to use next parameters:\n\t1). Username. \nFor example:\n\t$0 testuser"
+
+if [ ! $1 ]; then echo -e $usage; exit 0; fi
+
+STORE_DIR='/backups/.deleted/'
+MYSQLPWD=`cat /root/.mysql-root-password`
+DATE=`date +%Y-%m-%d_%H-%M`
+USER=$1
+
+if [ ! -d /var/www/$USER ]
+then
+        echo "Directory /var/www/$USER not exist!"
+        exit 0
+fi
+
+echo 'Press Enter to continue (Ctrl+C to exit)!'
+read
+
+rm -f /etc/httpd/conf/vhosts/$USER.conf
+/etc/init.d/httpd restart
+tar zcf $STORE_DIR$USER-$DATE-files.tar.gz /var/www/$USER
+for DB in `mysql -p$MYSQLPWD -B -N -e "select Db from mysql.db where user = '$USER'"`
+do
+        mysqldump -p$MYSQLPWD $DB | gzip > $STORE_DIR$DB-db-$DATE.sql.gz
+        mysql -p$MYSQLPWD -B -N -e "drop database $DB"
+done
+
+killall -9 -u $USER
+userdel -r $USER
+mysql -p$MYSQLPWD -B -N -e "DROP USER $USER@localhost"
