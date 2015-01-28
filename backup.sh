67c88a78bdcf4f478db86517f7dd06edf89b568b
Alexey Kostin <kostin@users.noreply.github.com>
2015-01-28 23:37:03 +0300

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+PASS=`cat /root/mysql-root-password.txt`
+DATE=`date +%Y-%m-%d_%H-%M`
+
+cd /var/www
+for i in `/bin/ls /var/www/ | grep -v 'html\|cgi-bin\|error\|icons'`
+do
+	tar zcf /backups/$i-files-$DATE.tar.gz $i
+	for k in `mysql -p$PASS -B -N -e "show databases" | grep $i`
+	do
+		mysqldump -p$PASS $k | gzip > /backups/$k-db-$DATE.sql.gz
+	done
+done
+
+cd /backups
+for i in `find . -mtime +60 -print | grep -v 'deleted'`
+do
+	rm -f $i
+done
