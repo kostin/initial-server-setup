5d53125526fa44ea53a35bfa79bf522ef37a64bf
Alexey Kostin <kostin@users.noreply.github.com>
2015-02-03 20:18:47 +0300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -6,7 +6,7 @@ DATE=`date +%Y-%m-%d_%H-%M`
 cd /var/www
 for i in `/bin/ls /var/www/ | grep -v 'html\|cgi-bin\|error\|icons'`
 do
-	tar cfzp /backups/$i-files-$DATE.tar.gz $i
+	tar cfzp --exclude=/var/www/$i/tmp --exclude=/var/www/$i/backups /backups/$i-files-$DATE.tar.gz /var/www/$i
 	for k in `mysql -p$PASS -B -N -e "show databases" | grep $i`
 	do
 		mysqldump -p$PASS $k | gzip > /backups/$k-db-$DATE.sql.gz
