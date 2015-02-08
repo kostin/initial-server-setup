2cbaefb3fc2da92f21b4b8dded3a2a5eac2b727b
Alexey Kostin <kostin@users.noreply.github.com>
2015-02-08 23:56:10 +0300

    Update hostdel.sh

--- hostdel.sh
+++ hostdel.sh
@@ -4,26 +4,29 @@ usage="To remove user you need to use next parameters:\n\t1). Username. \nFor ex
 
 if [ ! $1 ]; then echo -e $usage; exit 0; fi
 
-STORE_DIR='/backups/.deleted/'
 MYSQLPWD=`cat /root/.mysql-root-password`
 DATE=`date +%Y-%m-%d_%H-%M`
 USER=$1
 
 if [ ! -d /var/www/$USER ]
 then
-        echo "Directory /var/www/$USER not exist!"
+    	echo "Directory /var/www/$USER not exist!"
         exit 0
 fi
 
 echo 'Press Enter to continue (Ctrl+C to exit)!'
 read
 
+STORE_DIR='/backups/.deleted/'$USER
+
+mkdir -p $STORE_DIR
+
 rm -f /etc/httpd/conf/vhosts/$USER.conf
 /etc/init.d/httpd restart
-tar zcf $STORE_DIR$USER-$DATE-files.tar.gz /var/www/$USER
+tar cfzp $STORE_DIR/$USER-$DATE-files.tar.gz /var/www/$USER
 for DB in `mysql -p$MYSQLPWD -B -N -e "select Db from mysql.db where user = '$USER'"`
 do
-        mysqldump -p$MYSQLPWD $DB | gzip > $STORE_DIR$DB-db-$DATE.sql.gz
+  	mysqldump -p$MYSQLPWD $DB | gzip > $STORE_DIR/$DB-db-$DATE.sql.gz
         mysql -p$MYSQLPWD -B -N -e "drop database $DB"
 done
 
