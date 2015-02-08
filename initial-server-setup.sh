c1b3e63a335abbec6a4808609bb94a72de7760e5
Alexey Kostin <kostin@users.noreply.github.com>
2015-02-08 13:59:41 +0300

    Update initial-server-setup.sh

--- initial-server-setup.sh
+++ initial-server-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DLPATH='https://raw.githubusercontent.com/kostin/initial-server-setup/master'
+DLPATH='https://github.com/kostin/initial-server-setup/raw/master'
 
 function softinstall {
 	echo 'Installing software...'
