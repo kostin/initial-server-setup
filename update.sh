04338a158277c59d3a1720a7380a669eb70cff6b
Alexey Kostin <kostin@users.noreply.github.com>
2015-03-07 11:28:46 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,18 @@
+#!/bin/bash
+DLPATH='https://github.com/kostin/initial-server-setup/raw/master'
+
 cd /etc \
-&& wget -N https://raw.githubusercontent.com/kostin/initial-server-setup/master/my.cnf \
+&& wget -N $DLPATH/my.cnf \
 && touch /var/log/mysql-slow.log \
 && chmod 666 /var/log/mysql-slow.log \
 && service mysqld stop \
 && rm -f /var/lib/mysql/ib_logfile* \
 && service mysqld start
+
+cd /opt/scripts \
+&& wget -N $DLPATH/backup.sh \
+&& wget -N $DLPATH/hostadd.sh \
+&& wget -N $DLPATH/hostdel.sh \
+&& wget -N $DLPATH/hostexport.sh \
+&& wget -N $DLPATH/hostshow.sh \
+&& wget -N $DLPATH/hostdeploy.sh
