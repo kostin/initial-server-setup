e7e5066b6763f527e4f1792592b6d5ec5805fc7f
Alexey Kostin <kostin@users.noreply.github.com>
2015-03-12 13:49:30 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 DLPATH='https://github.com/kostin/initial-server-setup/raw/master'
 
-cd /etc \
-&& wget -N $DLPATH/my.cnf \
-&& touch /var/log/mysql-slow.log \
-&& chmod 666 /var/log/mysql-slow.log \
-&& service mysqld stop \
-&& rm -f /var/lib/mysql/ib_logfile* \
-&& service mysqld start
 
-cd /opt/scripts \
-&& wget -N $DLPATH/backup.sh \
-&& wget -N $DLPATH/hostadd.sh \
-&& wget -N $DLPATH/hostdel.sh \
-&& wget -N $DLPATH/hostexport.sh \
-&& wget -N $DLPATH/hostshow.sh \
-&& wget -N $DLPATH/hostdeploy.sh
+if [ "$1" = "mycnf" ] then;
+  cd /etc \
+  && wget -N $DLPATH/my.cnf \
+  && touch /var/log/mysql-slow.log \
+  && chmod 666 /var/log/mysql-slow.log \
+  && service mysqld stop \
+  && rm -f /var/lib/mysql/ib_logfile* \
+  && service mysqld start
+fi
+
+if [ "$1" = "scripts" ] then;
+  cd /opt/scripts \
+  && wget -N $DLPATH/backup.sh \
+  && wget -N $DLPATH/hostadd.sh \
+  && wget -N $DLPATH/hostdel.sh \
+  && wget -N $DLPATH/hostexport.sh \
+  && wget -N $DLPATH/hostshow.sh \
+  && wget -N $DLPATH/hostdeploy.sh
+fi
