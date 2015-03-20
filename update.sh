7015c01953018e3f6b5580b5450c24c643c2ffd4
Alexey Kostin <kostin@users.noreply.github.com>
2015-03-20 13:03:45 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -26,3 +26,14 @@ fi
 if [ "$1" = "key" ] && [ -f /root/.ssh/authorized_keys ]; then
   echo "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAvK2bwzvPOHAXnw8F7BYk7nC+f+pdyEys8LybGatrTfoBZUVLmV4qX3kEG9O9jC/pYiZyHqquGpcnr7L9dXmK7OGvE3GU4xMKWR6FJGbgjtg589vMA/w2q2c4OQ8Mfz4ryoLIuo8JEoS/lJJTjbzmfLc4aSwKvsz8nh/rJKmiNSYaCz20hsZ41YSsewFjOa7hdv5xX3u6hq8Bnd4m2Sm88BKT5E9oLg+lMIiYph5dW/GADZKdVppt/+B62iKAFyWZxK0BpefSJdXkrrwoXEJKCoN/+UVxsB0dWnVuhzsYt1yLk3AJgcLF/UP7J4aF+PzHv84vGO3Z/N2El4piB/W16w== root@renter" >> /root/.ssh/authorized_keys
 fi
+
+if [ "$1" = "mysql55" ]; then
+  rm -rf /etc/my/cnf \
+  && cd /etc \
+  && wget --quiet -N $DLPATH/my.cnf
+  rpm -Uvh https://mirror.webtatic.com/yum/el6/latest.rpm
+  yum -y install mysql.`uname -i` yum-plugin-replace
+  yum -y replace mysql --replace-with mysql55w
+  service mysqld restart
+  mysql_upgrade -u root -p`cat /root/.mysql-root-password`   
+fi
