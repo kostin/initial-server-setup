b5f1644b1804a82f7a54967ced8d3667098d6f61
Alexey Kostin <kostin@users.noreply.github.com>
2015-04-05 13:05:11 +0300

    Update hostplot.sh

--- hostplot.sh
+++ hostplot.sh
@@ -15,12 +15,12 @@ set yrange [ 0 : ]
 set grid
 set key under
 
-plot '/var/log/hoststat.dat' using 1:2 t 'Cpu usage %' with lines, \
+plot '/var/log/hoststat.dat' using 1:2 t 'Cpu usage %' with lines lw 2, \
      '/var/log/hoststat.dat' using 1:3 t 'Load Average' with lines, \
      '/var/log/hoststat.dat' using 1:4 t 'Memory usage %' with lines, \
-     '/var/log/hoststat.dat' using 1:5 t 'Swap usage %' with lines, \
-     '/var/log/hoststat.dat' using 1:6 t 'Disk space usage %' with lines, \
+     '/var/log/hoststat.dat' using 1:5 t 'Swap usage %' with lines lw 2, \
+     '/var/log/hoststat.dat' using 1:6 t 'Disk space usage %' with lines lw 2, \
      '/var/log/hoststat.dat' using 1:7 t 'IO wait %' with lines, \
      '/var/log/hoststat.dat' using 1:8 t 'RX, Mb/s' with lines, \
      '/var/log/hoststat.dat' using 1:9 t 'TX, Mb/s' with lines, \
-     '/var/log/hoststat.dat' using 1:10 t 'PHP mail queue x 10' with lines
+     '/var/log/hoststat.dat' using 1:10 t 'PHP mail queue x 10' with lines lw 2
