0c40116dcc7951f6178d78350614d35341a65dac
Alexey Kostin <kostin@users.noreply.github.com>
2015-04-05 13:02:37 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -150,10 +150,13 @@ if [ "$1" = "addhoststat" ]; then
   wget --quiet -N $DLPATH/hostplot.sh 
   chmod +x /opt/scripts/*.sh
   touch /var/log/hoststat.dat
-  echo "*/3 * * * * root /opt/scripts/hoststat.sh >> /var/log/hoststat.dat" > /etc/cron.d/hoststat
-  echo "*/15 * * * * root /opt/scripts/hostplot.sh > /var/www/000default/public/graph.svg" >> /etc/cron.d/hoststat
+  echo "*/3 * * * * root /opt/scripts/hoststat.sh > /dev/null" > /etc/cron.d/hoststat
+  echo "*/12 * * * * root /opt/scripts/hostplot.sh > /var/www/000default/public/graph.svg" >> /etc/cron.d/hoststat
   sed -i 's|*/10|*/3|g' /etc/cron.d/sysstat
   service crond restart
+  cd /etc/logrotate.d/
+  wget --quiet -N $DLPATH/hoststat.logrotate
+  logrotate --force /etc/logrotate.d/hoststat.logrotate
 fi
 
 service monit start
