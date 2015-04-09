ef326abd0a1df46362c33673a50a53c81284db0a
Alexey Kostin <kostin@users.noreply.github.com>
2015-04-09 14:15:23 +0300

    Create hostplotproc.sh

---
+++ hostplotproc.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/gnuplot
+
+reset
+set terminal svg size 960,480 dashed rounded enhanced
+
+set title "Server `hostname` with IP `hostname -i`. Cores: `grep -c ^processor /proc/cpuinfo`. Memory: `grep MemTotal /proc/meminfo | awk '{print $2/1024}'` MB. \nLocal graph date: `date`\nUptime: `uptime`"
+
+set xdata time
+set timefmt "%Y-%m-%d_%H:%M:%S"
+set format x "%H:%M"
+
+set yrange [ 0 : 130 ]
+set ytics 50
+set grid lt 8 lw 1 lc rgb "#bbbbbb"
+set key under
+
+set style fill solid 0.4 noborder
+
+plot '/var/log/hoststat.dat' using 1:2 t 'Apache CPU Usage %' with filledcurves y1=0 lc rgb "#cc0066", \
+     '/var/log/hoststat.dat' using 1:3 t 'Apache Memory Usage %' with lines lt 8 lw 1 lc rgb "#ff0000", \
+     '/var/log/hoststat.dat' using 1:4 t 'Apache Procs Num' with filledcurves y1=0 lc rgb "#0000ff", \
+     '/var/log/hoststat.dat' using 1:5 t 'MariaDB CPU Usage %' with lines lt 1 lw 1 lc rgb "#0000ff", \
+     '/var/log/hoststat.dat' using 1:6 t 'MariaDB Memory Usage %' with lines lt 1 lw 2 lc rgb "#ff9900"
