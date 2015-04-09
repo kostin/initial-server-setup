5c0993a465fdc9c91d4ddd66589b706b10650f2e
Alexey Kostin <kostin@users.noreply.github.com>
2015-04-09 15:48:38 +0300

    Create hostcopy.sh

---
+++ scripts/hostcopy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+RSERV=$1
+RSITE=$2
+LPATH="$(pwd)/${RSITE}/"
+if [ $3 ]; then LPATH=$3; fi
+
+if [ ! $1 ] || [ ! $2 ]; then 
+  echo "You have to set 2 parameters: host and remote site user. Also you can set 3rd parametr with local path for remote files"
+  echo "By default they stores in your current path in new dir with remote site name"
+  exit 0
+fi
+
+ssh root@${RSERV} "test -d /var/www/$RSITE"
+RTEST=$?
+
+if [ ${RTEST} -ne 0 ]; then
+  echo "Problem with remote server or site"
+  exit 1
+fi
+
+RSQLPASS=$(ssh root@${RSERV} "cat /root/.mysql-root-password")
+RBASES=$(ssh root@${RSERV} "mysql -p${RSQLPASS} -B -N -e \"select Db from mysql.db where user = '${RSITE}'\"")
+
+for DB in ${RBASES}
+do
+  ssh root@${RSERV} "mysqldump --force -p${RSQLPASS} $DB | gzip > /var/www/${RSITE}/$DB-db.sql.gz"
+done
+
+rsync -azh --delete -e ssh root@${RSERV}:/var/www/${RSITE}/ ${LPATH}
