0feee34c2557b6d3eff3df66f98f73f5644a2fc2
Alexey Kostin <kostin@users.noreply.github.com>
2015-04-11 22:54:20 +0300

    Update hostplotproc.sh

--- hostplotproc.sh
+++ hostplotproc.sh
@@ -11,13 +11,14 @@ set format x "%H:%M"
 
 set yrange [ 0 : 130 ]
 set ytics 50
-set grid lt 8 lw 1 lc rgb "#bbbbbb"
+set grid lt 8 lw 1 lc rgb "#dddddd"
 set key under
 
 set style fill solid 0.4 noborder
 
 plot '/var/log/hoststatproc.dat' using 1:4 t 'httpd Procs Num' with filledcurves y1=0 lc rgb "gold", \
-     '/var/log/hoststatproc.dat' using 1:6 t 'mysqld Mem Usage %' with lines lt 1 lw 3 lc rgb "turquoise", \
-     '/var/log/hoststatproc.dat' using 1:3 t 'httpd Mem Usage %' with lines lt 1 lw 3 lc rgb "coral", \
-     '/var/log/hoststatproc.dat' using 1:5 t 'mysqld CPU Usage %' with lines lt 1 lw 1 lc rgb "navy", \
-     '/var/log/hoststatproc.dat' using 1:2 t 'httpd CPU Usage %' with lines lt 1 lw 1 lc rgb "blue"
+     '/var/log/hoststatproc.dat' using 1:6 t 'mysqld Mem Usage %' with lines lt 1 lw 2 lc rgb "turquoise", \
+     '/var/log/hoststatproc.dat' using 1:3 t 'httpd Mem Usage %' with lines lt 1 lw 2 lc rgb "coral", \
+     '/var/log/hoststatproc.dat' using 1:5 t 'mysqld CPU Usage %' with lines lt 1 lw 1 lc rgb "blue", \
+     '/var/log/hoststatproc.dat' using 1:2:(10.0) t 'httpd CPU Usage %' with lines lt 1 lw 1 lc rgb "magenta", \
+     100 t '' with lines lt 3 lw 1 lc rgb "red"
