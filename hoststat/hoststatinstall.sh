a631684e7be5cde5df901fa3828910d831f5b0d3
Alexey Kostin <kostin@users.noreply.github.com>
2015-11-02 09:55:05 +0300

    Update hoststatinstall.sh

--- hoststat/hoststatinstall.sh
+++ hoststat/hoststatinstall.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-yum -y install sysstat
+yum -y install sysstat gnuplot
+cd /opt/scripts/hoststat
+chmod +x /opt/scripts/*.sh
+touch /var/log/hoststat.dat
+touch /var/log/hoststatproc.dat
+
+echo "*/3 * * * * root /opt/scripts/hoststat/hoststat.sh > /dev/null" > /etc/cron.d/hoststat
+echo "*/10 * * * * root /opt/scripts/hoststat/hostplot.sh > /var/www/000default/public/graph.svg" >> /etc/cron.d/hoststat
+echo "* * * * * root /opt/scripts/hoststat/hoststatproc.sh > /dev/null" > /etc/cron.d/hoststatproc
+echo "*/10 * * * * root /opt/scripts/hoststat/hostplotproc.sh > /var/www/000default/public/graph-proc.svg" >> /etc/cron.d/hoststatproc
+sed -i 's|*/10|*/3|g' /etc/cron.d/sysstat
+service crond restart
+
+cd /etc/logrotate.d/
+wget --quiet -N $DLPATH/hoststat/hoststat.logrotate
+logrotate --force /etc/logrotate.d/hoststat.logrotate
+wget --quiet -N $DLPATH/hoststat/hoststatproc.logrotate
+logrotate --force /etc/logrotate.d/hoststatproc.logrotate  
+
+/opt/scripts/hoststat/hostplot.sh > /var/www/000default/public/graph.svg
+/opt/scripts/hoststat/hostplotproc.sh > /var/www/000default/public/graph-proc.svg  
