4ad7469bbbd2719225bc79623732b8e977b38cac
Alexey Kostin <kostin@users.noreply.github.com>
2016-09-24 20:09:38 +0300

    Create hoststatproc.sh

---
+++ hoststatproc.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+DATFILE="/var/log/hoststatproc.dat"
+LOGFILE="/var/log/hoststatproc.dat"
+
+TIME=0
+TIMES=58
+LINE=""
+
+while [ $TIME -lt $TIMES ]; do
+  HTTPDUSG=$(ps -C httpd.itk -o "comm %cpu %mem" --no-headers)
+  #LINE="#"${HTTPDUSG}
+  if [ -z "${HTTPDUSG}" ]; then
+    HTTPDUSG="-0.0 -0.0 0"
+  else
+    HTTPDUSG=$(echo "${HTTPDUSG}" | grep -v defunc | awk '{a[$1] = $1; b[$1] += $2; c[$1] += $3; count ++} END {for (i in a) printf "%0.1f %0.1f %s\n", b[i], c[i], count}')
+  fi
+  MYSQLDUSG=$(ps -C mysqld -o "comm %cpu %mem" --no-headers)
+  #LINE=${LINE}"\n#"${MYSQLDUSG}
+  if [ -z "${MYSQLDUSG}" ]; then
+    MYSQLDUSG="-0.0 -0.0"
+  else
+    MYSQLDUSG=$(echo "${MYSQLDUSG}" | grep -v defunc | awk '{a[$1] = $1; b[$1] += $2; c[$1] += $3} END {for (i in a) printf "%0.1f %0.1f\n", b[i], c[i]}')
+  fi
+  LINE=${LINE}${TIME}" "${HTTPDUSG}" "${MYSQLDUSG}"\n"
+  ((TIME++))
+  sleep 1
+done
+
+DATE=$(date +'%Y-%m-%d_%H:%M:%S')
+AVG=$(echo ${LINE} | awk '{a[$1] = $1; b[$1] += $2; c[$1] += $3; d[$1] += $4; e[$1] += $5; f[$1] += $6; count ++} END {for (i in a) printf "%0.1f %0.1f %s %0.1f %0.1f\n", b[i]/count, c[i]/count, d[i]/count, e[i]/count, f[i]/count}')
+LINE=${DATE}" "${AVG}
+
+touch ${DATFILE}
+touch ${LOGFILE}
+
+echo "$LINE"
+echo "$LINE" >> ${DATFILE}
+echo "$LINE" >> ${LOGFILE}
+
+if [ $(cat ${DATFILE} | wc -l) -gt 4500 ]; then
+  TMPFILE="/tmp/hoststatproc.$$.tmp"
+  cat ${DATFILE} | tail -1500 > $TMPFILE
+  cat $TMPFILE > ${DATFILE}
+  rm -f $TMPFILE
+fi
