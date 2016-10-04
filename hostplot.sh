a81546b46aef8e2c711c20bf6c099060cc2c1b30
Alexey Kostin <kostin@users.noreply.github.com>
2016-10-04 16:28:34 +0300

    Create hostplot.sh

---
+++ hostplot.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/gnuplot
+reset
+set terminal svg size 960,480
+
+set title "Server usage `hostname`"
+
+set xdata time
+set timefmt "%Y-%m-%d_%H:%M:%S"
+set format x "%H:%M"
+set xlabel "Time"
+#set ylabel "Percents %"
+
+set yrange [ 0 : ]
+set grid
+set key under
+
+plot '/var/log/hoststat.dat' using 1:2 t 'Cpu usage %' with lines 3, \
+     '/var/log/hoststat.dat' using 1:3 t 'Load Average' with lines, \
+     '/var/log/hoststat.dat' using 1:4 t 'Memory usage %' with lines, \
+     '/var/log/hoststat.dat' using 1:5 t 'Swap usage %' with lines, \
+     '/var/log/hoststat.dat' using 1:6 t 'Disk space usage %' with lines, \
+     '/var/log/hoststat.dat' using 1:7 t 'IO wait %' with lines, \
+     '/var/log/hoststat.dat' using 1:8 t 'RX, Mb/s' with lines, \
+     '/var/log/hoststat.dat' using 1:9 t 'TX, Mb/s' with lines
