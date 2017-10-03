568bb590e30025beaa408a1d8eb074b2b842c00e
Alexey Kostin <kostin@users.noreply.github.com>
2017-10-03 22:43:58 +0300

    Delete hostssl.sh

--- hostssl.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p /var/www/$USER/.hostconf/.ssl
-
-/root/.acme.sh/acme.sh --issue $SSLDOMAINS -w /var/www/certs
-
-/root/.acme.sh/acme.sh --install-cert -d $SSLDOMAINS \
---key-file /var/www/$USER/.hostconf/.ssl/$USER.key
---fullchain-file /var/www/$USER/.hostconf/.ssl/$USER.fullchain.cer \
---reloadcmd "service nginx force-reload"
